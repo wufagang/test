4e64ddfb0ac90314215750921cc2dc44bca5ba9a
wufagang <wufagang@jd.com>
2024-11-05 11:38:14 +0800

    11111

--- src/main/java/com/wfg/demo/DockerBootApplication.java
+++ src/main/java/com/wfg/demo/DockerBootApplication.java
@@ -1,15 +1,18 @@
 package com.wfg.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author wufagang
  * @description
  * @date 2024年11月05日 08:29
  */
 @SpringBootApplication
-public class DockerBootApplication{
-    public static void main(String[] args)
-    {
-        SpringApplication.run(DockerBootApplication.class, args);
-    }
+public class DockerBootApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerBootApplication.class, args);
+    }
 
 }
--- src/main/java/com/wfg/demo/controller/OrderController.java
+++ src/main/java/com/wfg/demo/controller/OrderController.java
@@ -1,25 +1,29 @@
 package com.wfg.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 /**
  * @author wufagang
  * @description
  * @date 2024年11月05日 08:37
  */
 @RestController
-public class OrderController{
+public class OrderController{
 
-    @Value("${server.port}")
-    private String port;
+@Value("${server.port}")
+private String port;
 
-    @RequestMapping("/order/docker")
-    public String helloDocker()
-    {
-        return "hello docker"+"\t"+port+"\t"+ UUID.randomUUID().toString();
-    }
+@RequestMapping("/order/docker")
+    public String helloDocker() {
+        return"hello docker"+"\t"+port+"\t"+ UUID.randomUUID().toString();
+    }
 
-    @RequestMapping(value ="/order/index",method = RequestMethod.GET)
-    public String index()
-    {
-        return "服务端口号: "+"\t"+port+"\t"+UUID.randomUUID().toString();
-    }
+    @RequestMapping(value ="/order/index",method = RequestMethod.GET)
+    public String index(){
+        return"服务端口号: "+"\t"+port+"\t"+UUID.randomUUID().toString();
+    }
 }
